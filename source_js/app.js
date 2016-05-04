de3ace2f3f623861cd6796451da099c252ef34af
Vyshnav Kakivaya <vyshnav@msn.com>
2016-05-03 23:16:36 -0500

    In the process of hooking up Passport authentication with the app.

--- source_js/app.js
+++ source_js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mp4', ['ngRoute', 'mp4Controllers', 'mp4Services']);
+var app = angular.module('final-project', ['ngRoute', 'mp4Controllers', 'mp4Services']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
